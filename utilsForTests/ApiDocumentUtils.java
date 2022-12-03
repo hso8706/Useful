71fd854c0e718b2f9df92db536b479b820512014
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2022-12-03 17:01:53 +0900

    Update ApiDocumentUtils.java

--- utilsForTests/ApiDocumentUtils.java
+++ utilsForTests/ApiDocumentUtils.java
@@ -1,5 +1,3 @@
-package com.example.diary.utils;
-
 import org.springframework.restdocs.operation.OperationResponse;
 import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
 import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
